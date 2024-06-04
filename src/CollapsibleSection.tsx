9a6e2da3c55aa7d4e98cf737cdf3a1ebdfa3a9c0
Eduardo Iriarte Gahete <eiriarte@gmail.com>
2024-06-04 09:19:41 +0200

    Improve accessibility of CollapsibleSection

--- src/CollapsibleSection.tsx
+++ src/CollapsibleSection.tsx
@@ -1,15 +1,13 @@
-import { useState, useRef } from "react";
+import { useState, useRef, useContext } from "react";
 import "./styles.css";
-
-interface CollapsibleSectionProps {
-  heading: string;
-  children: React.ReactNode;
-}
+import { LocaleContext } from "./I18NResume";
 
 export default function CollapsibleSection({
+  id,
   heading,
   children,
-}: CollapsibleSectionProps) {
+}: React.PropsWithChildren<{ id: string; heading: string }>) {
+  const localeContext = useContext(LocaleContext);
   const contentRef = useRef<HTMLDivElement>(null);
   const [isOpen, setOpen] = useState(true);
   const onTransitionEnd = () => {
@@ -38,15 +36,63 @@ export default function CollapsibleSection({
     contents.addEventListener("transitionend", onTransitionEnd);
   };
 
+  const downCaret = (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      role="img"
+      aria-label={localeContext.strings.expand}
+      fill="none"
+      height={24}
+      width={24}
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      className="w-6 h-6"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="m19.5 8.25-7.5 7.5-7.5-7.5"
+      />
+    </svg>
+  );
+
+  const upCaret = (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      role="img"
+      aria-label={localeContext.strings.collapse}
+      fill="none"
+      height={24}
+      width={24}
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      className="w-6 h-6"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="m4.5 15.75 7.5-7.5 7.5 7.5"
+      />
+    </svg>
+  );
+
   return (
-    <section id="contact" className="mb-4 md:mb-6 last:lg:mb-0">
+    <section className="mb-4 md:mb-6 last:lg:mb-0">
       <h2 className="flex place-content-between border-b-2 border-indigo-500 font-medium text-xl md:text-2xl mb-2 uppercase text-indigo-500">
         {heading}
-        <button className="hover:text-indigo-300" onClick={toggleOpen}>
+        <button
+          className="hover:text-indigo-300"
+          onClick={toggleOpen}
+          aria-controls={id}
+          aria-expanded={isOpen ? "true" : "false"}
+        >
           {isOpen ? upCaret : downCaret}
         </button>
       </h2>
       <div
+        id={id}
         ref={contentRef}
         className="overflow-hidden duration-200 ease-out"
         style={{ height: isOpen ? "auto" : "0px" }}
@@ -56,41 +102,3 @@ export default function CollapsibleSection({
     </section>
   );
 }
-
-var downCaret = (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    fill="none"
-    height={24}
-    width={24}
-    viewBox="0 0 24 24"
-    strokeWidth={1.5}
-    stroke="currentColor"
-    className="w-6 h-6"
-  >
-    <path
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      d="m19.5 8.25-7.5 7.5-7.5-7.5"
-    />
-  </svg>
-);
-
-var upCaret = (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    fill="none"
-    height={24}
-    width={24}
-    viewBox="0 0 24 24"
-    strokeWidth={1.5}
-    stroke="currentColor"
-    className="w-6 h-6"
-  >
-    <path
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      d="m4.5 15.75 7.5-7.5 7.5 7.5"
-    />
-  </svg>
-);
